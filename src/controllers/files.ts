c415670d25e3c14b084c9d867334786c1d09ff1e
Vladislav Kokosh <v.kokosh@pixelplex.io>
2023-10-02 16:56:02 +0300

    Add logic for upload files

--- src/controllers/files.ts
+++ src/controllers/files.ts
@@ -3,12 +3,13 @@ import { fileFromPath, makeFileObjects, storeFiles } from '../storage';
 import { toBytes32 } from '../utils';
 
 export const web3StorageUpload = async (req: Request, res: Response): Promise<void> => {
+  const { projectName, projectDescription } = req.params;
   const reqFile = req.file;
 
   console.log(`Uploading image: [${reqFile.originalname}] to ipfs.`);
 
-  if (!reqFile) {
-    res.status(401).send({ message: 'invalid input' });
+  if (!reqFile || !projectName || !projectDescription) {
+    res.status(401).send({ message: 'Invalid input' });
   }
 
   const imageName = `${toBytes32(new Date().getTime().toString() + '_' + reqFile.originalname)}`;
@@ -17,7 +18,12 @@ export const web3StorageUpload = async (req: Request, res: Response): Promise<vo
 
   const imageCid = await storeFiles(file);
 
-  const files = await makeFileObjects(imageName, `https://${imageCid}.ipfs.w3s.link/${imageName}`);
+  const files = await makeFileObjects(
+    imageName,
+    `https://${imageCid}.ipfs.w3s.link/${imageName}`,
+    projectName,
+    projectDescription
+  );
 
   const metaDataCid = await storeFiles(files);
 
